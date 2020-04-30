31582574bbe9b8dadc64d623cecb75f0df44f13c
Сандал Винокуров <sandal.skv12@gmail.com>
2020-05-01 00:21:40 +0900

    first commit

---
+++ TetrisSFML/Application.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Main.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+
+using namespace sf;
+
+namespace app{
+	class Application {
+	private:
+		sf::RenderWindow* window;
+	public:
+		Application();
+		~Application();
+		void run();
+	};
+}
